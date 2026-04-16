6abd5956ecf870b2dd251a4fb8af1438b16ac0eb
Arkner-Industries <hornesfer@gmail.com>
2026-04-16 19:05:10 -0300

    NuevosCambios

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-const express = require("express")   //llamamos al modulo express
-const servidor = express()           //le paso la configuracion del servidor a la variable servidor
-servidor.listen(4000)                //El puerto del servidor
+const express = require("express")                         //llamamos al modulo express
+const path =require("path")
+const ruta = path.join(__dirname,"frontend")               // "__dirname" sirve para enviar al servidor la direccion del directorio que esto usando
+const servidor = express()                                 //le paso la configuracion del servidor a la variable servidor
+servidor.listen(4000)                                      //El puerto del servidor
+servidor.use(express.static(ruta))
+servidor.use(express.json())
 servidor.get("/",(req,res)=>{
-    res.send(__dirname)              // "__dirname" sirve para enviar al servidor la direccion del directorio que esto usando
+    res.send()             
+})
+servidor.post("/login",(req,res)=>{
+    console.log(req.body)
+    res.send("ok")
 })
