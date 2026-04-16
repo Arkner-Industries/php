4b0669af451b01157cb1e1a290edbcb83d7e46dd
Arkner-Industries <hornesfer@gmail.com>
2026-04-16 17:49:29 -0300

    Cambios

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require("express") //llamamos al modulo express
-const servidor = express() //le paso la configuracion del servidor a la variable servidor
-servidor.listen(4000) //El puerto del servidor
+const express = require("express")   //llamamos al modulo express
+const servidor = express()           //le paso la configuracion del servidor a la variable servidor
+servidor.listen(4000)                //El puerto del servidor
 servidor.get("/",(req,res)=>{
-    res.send("alv funciona mi primera pagina, maricones que usan barrera con un adc, y escoria que usan adcs en top")
+    res.send(__dirname)              // "__dirname" sirve para enviar al servidor la direccion del directorio que esto usando
 })
