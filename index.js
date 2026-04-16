135aefe3d5e265943e9b58613c0f9d0039681441
Arkner-Industries <hornesfer@gmail.com>
2026-04-16 17:19:29 -0300

    NiceFunciona

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const express = require("express") //llamamos al modulo express
 const servidor = express() //le paso la configuracion del servidor a la variable servidor
 servidor.listen(4000) //El puerto del servidor
 servidor.get("/",(req,res)=>{
-    res.send("servidor ok")
+    res.send("alv funciona mi primera pagina, maricones que usan barrera con un adc, y escoria que usan adcs en top")
 })
