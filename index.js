dce8095935cfa03766d00430b2b525294c3c2dc4
Arkner-Industries <hornesfer@gmail.com>
2026-04-23 18:54:34 -0300

    Mas Cambios

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express")                         //llamamos al modulo express
 const path =require("path")
-const ruta = path.join(__dirname,"frontend")               // "__dirname" sirve para enviar al servidor la direccion del directorio que esto usando
+const ruta = path.join(__dirname,"frontend")               // "__dirname" sirve para mostrar o usar la ruta de la carpeta
 const servidor = express()                                 //le paso la configuracion del servidor a la variable servidor
 servidor.listen(4000)                                      //El puerto del servidor
 servidor.use(express.static(ruta))
 servidor.use(express.json())
-servidor.get("/",(req,res)=>{
+servidor.get("/",(req,res)=>{                              // ()=>{} funciones de llave
     res.send()             
 })
 
