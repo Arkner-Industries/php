1f87969ce96f8d5460a588ee2f1eb911fd597391
Arkner-Industries <hornesfer@gmail.com>
2026-05-07 19:31:28 -0300

    mas cambios

--- config.js
+++ config.js
@@ -3,6 +3,13 @@ const path =require("path")
 const ruta = path.join(__dirname,"frontend")               // "__dirname" sirve para mostrar o usar la ruta de la carpeta
 const servidor = express()                                 //le paso la configuracion del servidor a la variable servidor
 servidor.listen(4000)                                      //El puerto del servidor
+
+const router = express.Router()
+
+servidor.use(router)
 servidor.use(express.static(ruta))
 servidor.use(express.json())
 
+module.exports={
+    router,
+}
--- index.js
+++ index.js
@@ -1,58 +1,53 @@
-const express = require("express")                         //llamamos al modulo express
-const path =require("path")
-const ruta = path.join(__dirname,"frontend")               // "__dirname" sirve para mostrar o usar la ruta de la carpeta
-const servidor = express()                                 //le paso la configuracion del servidor a la variable servidor
-servidor.listen(4000)                                      //El puerto del servidor
-servidor.use(express.static(ruta))
-servidor.use(express.json())
-servidor.get("/",(req,res)=>{                              // ()=>{} funciones de llave
+const router = require("./config.js")
+
+router.get("/",(req,res)=>{                              // ()=>{} funciones de llave
     res.send()             
 })
 
-servidor.get("/kled",(req,res)=>{
+router.get("/kled",(req,res)=>{
     res.sendFile(path.join (ruta, "/campeon.html"))
 })
 
-servidor.get("/index",(req,res)=>{
+router.get("/index",(req,res)=>{
     res.sendFile(path.join(ruta, "/index.html"))
 })
 
-servidor.get("/trundle",(req,res)=>{
+router.get("/trundle",(req,res)=>{
     res.sendFile(path.join(ruta, "/trundle.html"))
 })
 
-servidor.get("/jax",(req,res)=>{
+router.get("/jax",(req,res)=>{
     res.sendFile(path.join(ruta, "/jax.html"))
 })
 
-servidor.get("/login",(req,res)=>{
+router.get("/login",(req,res)=>{
     res.sendFile(path.join(ruta, "/login.html"))
 })
 
 
 
 
-servidor.post("/login",(req,res)=>{
+router.post("/login",(req,res)=>{
     console.log(req.body)
     res.send("ok")
 })
 
-servidor.post("/darius",(req,res)=>{
+router.post("/darius",(req,res)=>{
     console.log(req.body)
     res.send("Uf fantasmal ignite, bien de pito corto")
 })
 
-servidor.post("/volibear",(req,res)=>{
+router.post("/volibear",(req,res)=>{
     console.log(req.body)
     res.send("Eres un parasito")
 })
 
-servidor.post("/teemo",(req,res)=>{
+router.post("/teemo",(req,res)=>{
     console.log(req.body)
     res.send("De tantos campeones, eliges una rata que caga hongos")
 })
 
-servidor.post("/yasuo",(req,res)=>{
+router.post("/yasuo",(req,res)=>{
     console.log(req.body)
     res.send("En el late se gana, en efecto, tienes retraso")
 })
