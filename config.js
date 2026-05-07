b89e444857a40a1ef0f4946153e4d9229125e3cd
Arkner-Industries <hornesfer@gmail.com>
2026-05-07 18:58:55 -0300

    cambios

---
+++ config.js
@@ -0,0 +1,8 @@
+const express = require("express")                         //llamamos al modulo express
+const path =require("path")
+const ruta = path.join(__dirname,"frontend")               // "__dirname" sirve para mostrar o usar la ruta de la carpeta
+const servidor = express()                                 //le paso la configuracion del servidor a la variable servidor
+servidor.listen(4000)                                      //El puerto del servidor
+servidor.use(express.static(ruta))
+servidor.use(express.json())
+
